344723f4dbb71b17786c4abc703a9693fc8417f2
Alex Wolf <alexrudiwolf@gmail.com>
2021-03-27 19:33:37 -0400

    initial commit

---
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ WiredBrainCoffeeSurveys.Reports/Q1Results.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    public static class Q1Results
+    {
+        // Aggregate ratings
+        public static double ServiceScore { get; set; } = 8.0;
+
+        public static double CoffeeScore { get; set; } = 8.5;
+
+        public static double PriceScore { get; set; } = 6.0;
+
+        public static double FoodScore { get; set; } = 7.5;
+
+        public static double WouldRecommend { get; set; } = 6.5;
+
+        public static string FavoriteProduct { get; set; } = "Cappucino";
+
+        public static string LeastFavoriteProduct { get; set; } = "Granola";
+
+        // Aggregate counts
+        public static double NumberSurveyed { get; set; } = 500;
+
+        public static double NumberResponded { get; set; } = 325;
+
+        public static double NumberRewardsMembers { get; set; } = 130;
+    }
+}
