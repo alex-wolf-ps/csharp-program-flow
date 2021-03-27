aad46c223d85d1914c67dd31367c9bc3718391c8
Alex Wolf <alexrudiwolf@gmail.com>
2021-03-27 19:34:18 -0400

    end of module 02

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,7 +6,23 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            // Calculated Values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall Score: {overallScore}");
+
+            // Logical comparisons
+            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
         }
     }
 }
