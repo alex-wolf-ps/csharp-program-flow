da1b0d97ef70f607d15467bcdcfdbf22dff487da
Alex Wolf <alexrudiwolf@gmail.com>
2021-04-08 14:03:15 -0400

    end

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,23 +8,64 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GenerateTasksReport();
+        }
+
+        public static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+
             // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            Console.WriteLine($"Response Percentage: {responseRate}");
-            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScore}");
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
 
-            // Logical comparisons
-            bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
-            bool customersRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement ideas.");
+            }
 
-            Console.WriteLine($"Coffee Score Higher Than Food: {isCoffeeScoreLower}");
-            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coffee coupon.");
+            }
+            else
+            {
+                tasks.Add("Rewards participants with discount coffee coupon.");
+            }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals.");
+                    break;
+
+                case "Cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+
+                case var improve when improve.Length > 5:
+                    tasks.Add("Stuff.");
+                    break;
+                case var test2 when test2.Length > 6:
+                    tasks.Add("Stuff.");
+                    break;
+                default:
+                    tasks.Add("Investigate individual comments for ideas.");
+                    break;
+            }
         }
     }
 }
--- WiredBrainCoffeeSurveys.Reports/Q1Results.cs
+++ WiredBrainCoffeeSurveys.Reports/Q1Results.cs
@@ -9,7 +9,7 @@ namespace WiredBrainCoffeeSurveys.Reports
         // Aggregate ratings
         public static double ServiceScore { get; set; } = 8.0;
 
-        public static double CoffeeScore { get; set; } = 8.5;
+        public static double CoffeeScore { get; set; } = 4.0;
 
         public static double PriceScore { get; set; } = 6.0;
 
@@ -20,6 +20,8 @@ namespace WiredBrainCoffeeSurveys.Reports
         public static string FavoriteProduct { get; set; } = "Cappucino";
 
         public static string LeastFavoriteProduct { get; set; } = "Granola";
+
+        public static string AreaToImprove { get; set; } = "MobileApp";
 
         // Aggregate counts
         public static double NumberSurveyed { get; set; } = 500;
