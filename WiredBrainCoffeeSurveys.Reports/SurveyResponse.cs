831e722fcc9d5092c627b2bfe5b6ebfbe473e790
Alex Wolf <alexrudiwolf@gmail.com>
2021-04-10 18:39:19 -0400

    start

---
+++ WiredBrainCoffeeSurveys.Reports/SurveyResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace WiredBrainCoffeeSurveys.Reports
+{
+    public class SurveyResponse
+    {
+        public double ServiceScore { get; set; }
+
+        public double CoffeeScore { get; set; }
+
+        public double PriceScore { get; set; }
+
+        public double FoodScore { get; set; }
+
+        public double WouldRecommend { get; set; }
+
+        public string FavoriteProduct { get; set; }
+
+        public string LeastFavoriteProduct { get; set; }
+
+        public string AreaToImprove { get; set; }
+
+        public bool IsRewardsMember { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
