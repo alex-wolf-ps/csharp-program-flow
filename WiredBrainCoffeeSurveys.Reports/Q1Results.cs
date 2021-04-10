4df0e71dba5ab5f449251b9786f0dc33f0dabf5d
Alex Wolf <alexrudiwolf@gmail.com>
2021-04-10 18:38:34 -0400

    temp

--- WiredBrainCoffeeSurveys.Reports/Q1Results.cs
+++ WiredBrainCoffeeSurveys.Reports/Q1Results.cs
@@ -29,5 +29,87 @@ namespace WiredBrainCoffeeSurveys.Reports
         public static double NumberResponded { get; set; } = 325;
 
         public static double NumberRewardsMembers { get; set; } = 130;
+
+        public static List<SurveyResponse> Responses = new List<SurveyResponse>()
+        {
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "MobileApp",
+                FavoriteProduct = "Latte",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 8.0,
+                Comments = "Coffee and service are great!"
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "MobileApp",
+                FavoriteProduct = "Cappucino",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 5.0,
+                Comments = "Your mobile app is not very user friendly and prices are too high."
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "Cleanliness",
+                FavoriteProduct = "Americano",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 8.0,
+                Comments = "The store I went into was not clean."
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "MobileApp",
+                FavoriteProduct = "Cappucino",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 4.0,
+                Comments = "Your prices are higher than the surrounding stores."
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "Other",
+                FavoriteProduct = "Iced Coffee",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 6.0,
+                Comments = "I like the stores but your coffee is too expensive."
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test@sample.com",
+                CoffeeScore = 8.0,
+                FoodScore = 7.0,
+                PriceScore = 6.5,
+                ServiceScore = 8.5,
+                AreaToImprove = "MobileApp",
+                FavoriteProduct = "Green tea macha",
+                LeastFavoriteProduct = "Fruit",
+                WouldRecommend = 7.0,
+                Comments = "Your staff are very friendly."
+            }
+        };
     }
 }
