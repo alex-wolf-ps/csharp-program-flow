5f83aced9bcf08876f1583663153dd364bfe8cc2
Alex Wolf <alexrudiwolf@gmail.com>
2021-04-20 21:54:42 -0400

    start

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -8,64 +6,23 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            GenerateTasksReport();
-        }
-
-        public static void GenerateTasksReport()
-        {
-            var tasks = new List<string>();
-
             // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
-            {
-                tasks.Add("Investigate coffee recipes and ingredients.");
-            }
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall Score: {overallScore}");
 
-            if (overallScore > 8.0)
-            {
-                tasks.Add("Work with leadership to reward staff");
-            }
-            else
-            {
-                tasks.Add("Work with employees for improvement ideas.");
-            }
+            // Logical comparisons
+            bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
 
-            if (responseRate < .33)
-            {
-                tasks.Add("Research options to improve response rate.");
-            }
-            else if (responseRate > .33 && responseRate < .66)
-            {
-                tasks.Add("Reward participants with free coffee coupon.");
-            }
-            else
-            {
-                tasks.Add("Rewards participants with discount coffee coupon.");
-            }
-
-            switch (Q1Results.AreaToImprove)
-            {
-                case "RewardsProgram":
-                    tasks.Add("Revisit the rewards deals.");
-                    break;
-
-                case "Cleanliness":
-                    tasks.Add("Contact the cleaning vendor.");
-                    break;
-
-                case var improve when improve.Length > 5:
-                    tasks.Add("Stuff.");
-                    break;
-                case var test2 when test2.Length > 6:
-                    tasks.Add("Stuff.");
-                    break;
-                default:
-                    tasks.Add("Investigate individual comments for ideas.");
-                    break;
-            }
+            Console.WriteLine($"Coffee Score Higher Than Food: {isCoffeeScoreLower}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
         }
     }
 }
