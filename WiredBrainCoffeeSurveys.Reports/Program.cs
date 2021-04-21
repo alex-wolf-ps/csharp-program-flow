62c59954f7417fccb9f2c5f48dd63dffddd23bab
Alex Wolf <alexrudiwolf@gmail.com>
2021-04-20 22:02:42 -0400

    'temp

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -7,6 +7,16 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GenerateTasksReport();
+        }
+
+        static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
             if (Q1Results.CoffeeScore < Q1Results.FoodScore)
             {
                 tasks.Add("Revisit recipes for quality and taste.");
@@ -18,7 +28,7 @@ namespace WiredBrainCoffeeSurveys.Reports
             {
                 Console.WriteLine("Todo: Use the funds to market the rewards program");
             }
-            else if(rewardsMemberPercentage > .25 && rewardsMemberPercentage < .75)
+            else if (rewardsMemberPercentage > .25 && rewardsMemberPercentage < .75)
             {
                 Console.WriteLine($"Todo: Use the funds to give coffee to the percent of loyal customers.");
             }
@@ -43,27 +53,6 @@ namespace WiredBrainCoffeeSurveys.Reports
                     Console.WriteLine("Check the comments to get ideas");
                     break;
             }
-        }
-
-        static void GenerateMetricsReport()
-        {
-            // Calculated Values
-            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
-            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
-
-            Console.WriteLine($"Response Percentage: {responseRate}");
-            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScore}");
-
-            // Logical comparisons
-            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            bool customersRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
-
-            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
-            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
         }
     }
 }
